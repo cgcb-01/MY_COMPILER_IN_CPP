68be8477d68e667ef038920bbfd8e114bf960cb9
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-09-10 00:51:06 +0530

    Input taking in main

--- llcheck.cpp
+++ llcheck.cpp
@@ -1,1 +1,28 @@
 #include<bits/stdc++.h>
+using namespace std;
+vector<char> terminals, non_terminals;
+vector<pair<char, vector<string>>> productions;
+int main()
+{
+    cout << "Insert the Terminals In Your Grammar:\n";
+    while(true)
+    {
+        char c;
+        cin >> c;
+        if(c==NULL)
+            break;
+        else
+            terminals.push_back(c);
+    }
+    cout << "Insert The Non-Terminals In your Grammar:\n";
+    while(true)
+    {
+        char c;
+        cin >> c;
+        if(c==NULL)
+            break;
+        else
+            non_terminals.push_back(c);
+    }
+    
+}
